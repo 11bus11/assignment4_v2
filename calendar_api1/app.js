f6355c0d7d9f4be887eb62ebed328c903c0e1a99
11bus11 <02linfor01@gmail.com>
2025-12-28 16:39:50 +0100

    aaah

--- calendar_api1/app.js
+++ calendar_api1/app.js
@@ -1,7 +1,13 @@
-
 import express, { json } from 'express';
 const app = express();
 const port = 3000;
+
+import { Sequelize, DataTypes } from '@sequelize/core';
+import { Event } from './event';
+//const Event = Event;
+const event = new Event("dnd4444", "Title1", "ghfjdkkssk", "2012-07-19T22:00:00Z", "2012-07-19T2300:00Z");
+
+
 
 app.use(json()); // Middleware to parse JSON requests
 
@@ -13,17 +19,20 @@ app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
 
-app.get('/events', (req, res) => {
-  const {id, context_code, title, description, start_at, end_at} = req.body;
-  const event = 
-    //{ id: 1, context_code: "473847", title: 'title1', description: "hfhfhfhf", start_at: '2026-07-19T21:00:00Z', end_at: '2012-07-19T22:00:00Z' },
+
+app.get('/events', async (req, res, re) => {
+  const {context_code, title, description, start_at, end_at} = req.body;
+  const
   res.json(event);
 });
 
 app.post('/events', (req, res) => {
-  const newEvent = {
-    id: 3,
-    name: req.body.name,
+  const event = {
+  context_code: context_code,
+  title: title,
+  description: description,
+  start_at: start_at,
+  end_at: end_at,
   };
   res.status(201).json(newUser);
 });
---
+++ calendar_api1/event.js
@@ -0,0 +1,15 @@
+class Event {
+    constructor(context_code, title, description, start_at, end_at) {
+        this.context_code = context_code;
+        this.title = title;
+        this.description = description;
+        this.start_at = start_at;
+        this.end_at = end_at;
+    }
+
+    get context_code() {
+    return this.context_code;
+  }
+}
+
+export class Event {};
